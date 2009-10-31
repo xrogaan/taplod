588f78f0fe7c32993b914098e117e9a7710c4110
Bellière Ludovic <xrogaan@gmail.com>
2009-10-31 09:47:47 +0100

    adding method pageExists() to Taplod_Loaded

--- library/Taplod/Loader.php
+++ library/Taplod/Loader.php
@@ -15,87 +15,94 @@ require_once 'Taplod/Exception.php';
  * @license http://opensource.org/licenses/mit-license.php MIT license
  */
 class Taplod_Loader {
-	/**
-	 * Load a class from a php file.
-	 */
-	public static function loadClass($class) {
-		if (class_exists($class, false) || interface_exists($class, false)) {
-			return false;
-		}
+/**
+ * Load a class from a php file.
+ */
+    public static function loadClass($class) {
+        if (class_exists($class, false) || interface_exists($class, false)) {
+            return false;
+        }
 
-		$file = str_replace('_',DIRECTORY_SEPARATOR,$class) . '.php';
+        $file = str_replace('_',DIRECTORY_SEPARATOR,$class) . '.php';
 
-		self::loadFile($file);
+        self::loadFile($file);
 
-		if (!class_exists($class, false) && !interface_exists($class, false)) {
-			require_once 'Taplod/Exception.php';
-			throw new Taplod_Exception("Class \"$class\" was not found in the source file \"$file\".");
-		}
+        if (!class_exists($class, false) && !interface_exists($class, false)) {
+            require_once 'Taplod/Exception.php';
+            throw new Taplod_Exception("Class \"$class\" was not found in the source file \"$file\".");
+        }
 
-	}
+    }
 
-	/**
-	 * Vérifie l'existance d'un fichier et l'inclus
-	 *
-	 * @param string $filename
-	 * @return void
-	 */
-	public static function loadFile($filename) {
-		$filename = trim($filename);
+    /**
+     * Vérifie l'existance d'un fichier et l'inclus
+     *
+     * @param string $filename
+     * @return void
+     */
+    public static function loadFile($filename) {
+        $filename = trim($filename);
 
-		$path = explode(PATH_SEPARATOR,ini_get('include_path'));
+        if (Taplod_Loader::fileExists($filename)) {
+            try {
+                include_once $filename;
+            } catch (exception $e) {
+                echo "error";
+                die($e->getMessage());
+            }
+            return;
+        }
+        require_once 'Taplod/Exception.php';
+        throw new Taplod_Exception("File '$filename' was not found.");
+    }
 
-		foreach ($path as $dir) {
-			$file = $dir.DIRECTORY_SEPARATOR.$filename;
-			if (file_exists($file)) {
-                                try {
-                                    include_once $file;
-                                } catch (exception $e) {
-                                    echo "error";
-                                    die($e->getMessage());
-                                }
-				return;
-			} else {
-                            echo "$file doesn't exists.";
-                        }
-		}
+    public static function fileExists($filename) {
+        $path = explode(PATH_SEPARATOR,ini_get('include_path'));
 
-		require_once 'Taplod/Exception.php';
-		throw new Taplod_Exception("File '$filename' was not found.");
-	}
+        foreach ($path as $dir) {
+            $file = $dir.DIRECTORY_SEPARATOR.$filename;
+            if (file_exists($file)) {
+                return true;
+            } else {
+                if (defined('APPLICATION_ENVIRONMENT') && APPLICATION_ENVIRONMENT == 'development')
+                    echo "$file doesn't exists.";
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * spl_autload() implementation
-	 */
-	public static function autoload($class) {
-		try {
-			@self::loadClass($class);
-			return $class;
-		} catch(Taplod_Exception $e) {
-			return false;
-		}
-	}
+    /**
+     * spl_autload() implementation
+     */
+    public static function autoload($class) {
+        try {
+            @self::loadClass($class);
+            return $class;
+        } catch(Taplod_Exception $e) {
+            return false;
+        }
+    }
 
-	/**
-	 * Register & unregister autoload class with spl_autload_(un)register
-	 *
-	 * @param string $class
-	 * @param boolean $enabled
-	 * @return void
-	 * @throws Taplod_Exception if spl_autoload() is not found
-	 */
-	public static function registerAutoload($class='Taplod_Loader', $enabled = true) {
-		if (!function_exists('spl_autoload')) {
-			require_once 'Taplod/Exception.php';
-			throw new Taplod_Exception('spl_autload doesn\'t exists in this php installation');
-		}
+    /**
+     * Register & unregister autoload class with spl_autload_(un)register
+     *
+     * @param string $class
+     * @param boolean $enabled
+     * @return void
+     * @throws Taplod_Exception if spl_autoload() is not found
+     */
+    public static function registerAutoload($class='Taplod_Loader', $enabled = true) {
+        if (!function_exists('spl_autoload')) {
+            require_once 'Taplod/Exception.php';
+            throw new Taplod_Exception('spl_autload doesn\'t exists in this php installation');
+        }
 
-		self::loadClass($class);
+        self::loadClass($class);
 
-		if ($enabled == true) {
-			spl_autoload_register(array($class, 'autoload'));
-		} else {
-			spl_autoload_unregister(array($class, 'autoload'));
-		}
-	}
+        if ($enabled == true) {
+            spl_autoload_register(array($class, 'autoload'));
+        } else {
+            spl_autoload_unregister(array($class, 'autoload'));
+        }
+    }
 }
